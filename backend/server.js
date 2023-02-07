af1d3e02acabde6401ba8e787eaf9f1baafcb941
Taham-Jan <m.taham0101@gmail.com>
2023-02-07 19:56:20 +0500

    new

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,18 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');;
+const path = require('path')
 const port = process.env.PORT || 5000;
 connectDB()
 
 const app = express();
 
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname,'../ngoweb/build')));
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,'../ngoweb/build/index.html'));
+})
 
 app.get("/", (req, res) => {
     res.json({ message: "API running..." });
