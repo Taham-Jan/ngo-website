a6db5e7a64e0ace5192368ca3708d5753f41883f
Taham-Jan <m.taham0101@gmail.com>
2023-02-07 22:05:31 +0500

    kk

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const app = express();
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname,'./ngoweb/build')));
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./ngoweb/build/index.html'));
+app.get('*',function(_,res){
+  res.sendFile(path.join(__dirname,'./ngoweb/build/index.html'),function(err){
+    res.status(500).send(err);
+  });
 })
 const port = process.env.PORT || 5000;
 connectDB()
